9e886db7eeb76cc76e06cb8309a05e91f44ba0a9
alexandre rosati <alex7170@gmail.com>
2014-11-30 15:06:32 +0100

    suppression du modéle ancien

--- src/modele/CRUD.java
+++
@@ -1,8 +0,0 @@
-package modele;
-
-public interface CRUD {
-	  public void create()throws Exception;
-	  public void read() throws Exception;
-	  public void update() throws Exception;
-	  public void delete() throws Exception;
-	}
