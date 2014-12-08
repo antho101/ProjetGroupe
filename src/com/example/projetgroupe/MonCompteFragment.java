354b3b47d59fcff671d87f7a8629ed632d6c4d0c
antho101 <rg512@live.be>
2014-12-08 19:41:28 +0100

    problème fragments

--- src/com/example/projetgroupe/MonCompteFragment.java
+++ src/com/example/projetgroupe/MonCompteFragment.java
@@ -1,24 +1,44 @@
-
 package com.example.projetgroupe;
 
+import modele.Session;
+
+import com.example.projetgroupe.ActivityInscription.MyAccesInscriptionDB;
+
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView.OnEditorActionListener;
 
 public class MonCompteFragment extends Fragment {
+	private EditText mEditText;
 
 	public MonCompteFragment() {
 	}
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+	private Button btn_mycompte_pseudochange = null;
 
-		View rootView = inflater.inflate(R.layout.fragment_moncompte, container,
-				false);
-		
-		return rootView;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		/*setContentView(R.layout.fragment_moncompte);
+
+		btn_mycompte_pseudochange = (Button) findViewById(R.id.btn_mycompte_pseudochange);
+		btn_mycompte_pseudochange
+				.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+					}
+				});*/
+
 	}
+
 }
